42c2855a1e6a88f7168c6f3db8dd6fa8680c92e4
DonPanino <130209471+DonPanino@users.noreply.github.com>
2023-04-09 21:02:29 +0930

    idk

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Don Is GOD'
+version '3.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua'
+}
+client_script 'client/main.lua'
+server_scripts  {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+    'html/profanity.js',
+    'html/script.js'
+}
+
+dependencies {
+    'qb-core',
+    'qb-spawn'
+}
+
+lua54 'yes'
